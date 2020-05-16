70bb10db03b1c5f9414895f7dcd3c9a710a3b625
tamaji <tamaji229@gmail.com>
2020-05-16 21:09:54 +0000

    routing grouping fixed

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// News Modelを使用する
+use App\News;
+
 class NewsController extends Controller
 {
     // addを追加
@@ -13,8 +16,31 @@ class NewsController extends Controller
         return view('admin.news.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
